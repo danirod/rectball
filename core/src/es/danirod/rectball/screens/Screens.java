959778e52ec9cb9a4a0aa63a4fe20a691900b26b
Dani Rodríguez <danirod@outlook.com>
2015-10-03 21:18:39 +0200

    Add Settings screen.

--- core/src/es/danirod/rectball/screens/Screens.java
+++ core/src/es/danirod/rectball/screens/Screens.java
@@ -15,6 +15,8 @@ public class Screens {
 
     public static final int LOADING = 5;
 
+    public static final int STATISTICS = 6;
+
     public static final int DEBUG = 10;
 
     public static final int COMBINATION_TEST = 11;
