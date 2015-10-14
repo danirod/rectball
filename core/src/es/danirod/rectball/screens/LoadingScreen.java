cbc57d8f445f7f3501cc4f594c5382367b9a43da
Dani Rodríguez <danirod@outlook.com>
2015-10-14 20:12:25 +0200

    Add splash screen

--- core/src/es/danirod/rectball/screens/LoadingScreen.java
+++ core/src/es/danirod/rectball/screens/LoadingScreen.java
@@ -19,25 +19,57 @@ package es.danirod.rectball.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Timer;
 import es.danirod.rectball.RectballGame;
+import es.danirod.rectball.actors.board.BoardActor;
+import es.danirod.rectball.model.BallColor;
+import es.danirod.rectball.model.Board;
 
 public class LoadingScreen extends AbstractScreen {
 
-    private ShapeRenderer shaper;
+    private static final float FADE_SPEED = 0.15f;
+
+    private static final float BALL_SPEED = 0.15f;
+
+    private Board board;
+
+    private BoardActor boardActor;
+
+    private Texture ballsTexture;
+
+    private boolean canUpdate;
+    private TextureAtlas ballAtlas;
 
     public LoadingScreen(RectballGame game) {
         super(game);
+        board = new Board(2);
+        board.getBall(0, 0).setColor(BallColor.BLUE);
+        board.getBall(0, 1).setColor(BallColor.RED);
+        board.getBall(1, 1).setColor(BallColor.GREEN);
+        board.getBall(1, 0).setColor(BallColor.YELLOW);
     }
 
     @Override
-    public void show() {
-        shaper = new ShapeRenderer();
-    }
+    public void setUpInterface(Table table) {
+        ballsTexture = new Texture("board/normal.png");
+        TextureRegion[][] regions = TextureRegion.split(ballsTexture, 256, 256);
+        ballAtlas = new TextureAtlas();
+        ballAtlas.addRegion("ball_red", regions[0][0]);
+        ballAtlas.addRegion("ball_yellow", regions[0][1]);
+        ballAtlas.addRegion("ball_blue", regions[1][0]);
+        ballAtlas.addRegion("ball_green", regions[1][1]);
+        ballAtlas.addRegion("ball_gray", regions[1][2]);
 
-    @Override
-    public void hide() {
-        shaper.dispose();
+        boardActor = new BoardActor(ballAtlas, board);
+        boardActor.setColoured(true);
+        table.add(boardActor).size(100).align(Align.center);
     }
 
     @Override
@@ -46,29 +78,73 @@ public class LoadingScreen extends AbstractScreen {
     }
 
     @Override
+    public void show() {
+        super.show();
+        canUpdate = false;
+        swapBalls();
+        getStage().addAction(Actions.sequence(
+                Actions.alpha(0),
+                Actions.alpha(1, FADE_SPEED),
+                Actions.run(new Runnable() {
+                    @Override
+                    public void run() {
+                        canUpdate = true;
+                    }
+                })
+        ));
+    }
+
+    @Override
     public void render(float delta) {
-        float width = Gdx.graphics.getWidth();
-        float height = Gdx.graphics.getHeight();
-
-        float barWidth = width * 0.8f;
-        float barHeight = barWidth * 0.1f;
-        float barX = (width - barWidth) / 2;
-        float barY = (height - barHeight) / 2;
-
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        if (game.manager.update()) {
-            game.finishLoading();
-        } else {
-            float progress = game.manager.getProgress();
-            shaper.setAutoShapeType(true);
-            shaper.begin();
-            shaper.set(ShapeRenderer.ShapeType.Line);
-            shaper.rect(barX - 5, barY - 5, barWidth + 10, barHeight + 10);
-            shaper.set(ShapeRenderer.ShapeType.Filled);
-            shaper.rect(barX, barY, barWidth * progress, barHeight);
-            shaper.end();
+        super.render(delta);
+        if (canUpdate && game.manager.update(1000 / 120)) {
+            canUpdate = false;
+            getStage().addAction(Actions.sequence(
+                    Actions.alpha(0, FADE_SPEED),
+                    Actions.delay(0.1f, Actions.run(new Runnable() {
+                        @Override
+                        public void run() {
+                            boardActor.remove();
+                            ballAtlas.dispose();
+                            ballsTexture.dispose();
+                            game.finishLoading();
+                        }
+                    }))
+            ));
         }
+    }
+
+    private void swapBalls() {
+        getStage().addAction(Actions.sequence(
+                Actions.run(new Runnable() {
+                    @Override
+                    public void run() {
+                        boardActor.getBall(0, 0).addAction(Actions.moveBy(50, 0, BALL_SPEED));
+                        boardActor.getBall(1, 0).addAction(Actions.moveBy(0, 50, BALL_SPEED));
+                        boardActor.getBall(1, 1).addAction(Actions.moveBy(-50, 0, BALL_SPEED));
+                        boardActor.getBall(0, 1).addAction(Actions.moveBy(0, -50, BALL_SPEED));
+                    }
+                }),
+                Actions.delay(BALL_SPEED * 2, Actions.run(new Runnable() {
+                            @Override
+                            public void run() {
+                                boardActor.getBall(0, 0).addAction(Actions.moveBy(-50, 0));
+                                boardActor.getBall(1, 0).addAction(Actions.moveBy(0, -50));
+                                boardActor.getBall(1, 1).addAction(Actions.moveBy(50, 0));
+                                boardActor.getBall(0, 1).addAction(Actions.moveBy(0, 50));
+                                BallColor bottomLeft = board.getBall(0, 0).getColor();
+                                BallColor bottomRight = board.getBall(1, 0).getColor();
+                                BallColor upperLeft = board.getBall(0, 1).getColor();
+                                BallColor upperRight = board.getBall(1, 1).getColor();
+
+                                board.getBall(0, 0).setColor(upperLeft);
+                                board.getBall(1, 0).setColor(bottomLeft);
+                                board.getBall(1, 1).setColor(bottomRight);
+                                board.getBall(0, 1).setColor(upperRight);
+
+                                swapBalls();
+                            }
+                        }))
+        ));
     }
 }
