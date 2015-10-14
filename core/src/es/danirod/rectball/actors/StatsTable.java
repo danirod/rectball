e197884720c1a2c83575acc3726ad931b739cfaf
Dani Rodríguez <danirod@outlook.com>
2015-10-14 18:48:44 +0200

    Change the settings screen

--- core/src/es/danirod/rectball/actors/StatsTable.java
+++ core/src/es/danirod/rectball/actors/StatsTable.java
@@ -1,11 +1,12 @@
 package es.danirod.rectball.actors;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Align;
 
-import java.util.Map;
+import java.util.*;
 
 import es.danirod.rectball.RectballGame;
 import es.danirod.rectball.statistics.StatisticSet;
@@ -27,29 +28,117 @@ public class StatsTable extends Table {
         this.title = title;
         this.data = data;
 
-        addDataSet("statistics.total", stats.getTotalData(), true);
-        addDataSet("statistics.color", stats.getColorData(), true);
-        addDataSet("statistics.sizes", stats.getSizesData(), false);
+        defaults().padBottom(30);
+        add(addBestScores()).fillX().expandX().row();
+        add(addTotalData()).fillX().expandX().row();
+        add(addColorData()).fillX().expandX().row();
+        add(addSizesData()).fillX().expandX().row();
     }
 
-    private void addDataSet(String title, StatisticSet set, boolean translateKeys) {
-        // Título de la sección.
-        add(new Label(game.getLocale().get(title), this.title)).colspan(2).row();
+    private Table addBestScores() {
+        Table best = new Table();
+        best.add(new Label(game.getLocale().get("statistics.best"), this.title)).colspan(2).row();
 
-        // Agrega las distintas propiedades.
+        // Add best score.
+        String bestScore = Long.toString(game.scores.getHighestScore());
+        best.add(new Label(game.getLocale().get("statistics.best.score"), data)).align(Align.left).fillX();
+        best.add(new Label(bestScore, data)).align(Align.right).expandX().row();
+
+        // Add best time.
+        // FIXME: This is not the best time.
+        String bestTime = secondsToTime(game.statistics.getTotalData().getValue("time"));
+        best.add(new Label(game.getLocale().get("statistics.best.time"), data)).align(Align.left).fillX();
+        best.add(new Label(bestTime, data)).align(Align.right).expandX().row();
+
+        return best;
+    }
+
+    private Table addTotalData() {
+        Table total = new Table();
+        total.add(new Label(game.getLocale().get("statistics.total"), this.title)).colspan(2).row();
+
+        StatisticSet set = game.statistics.getTotalData();
         for (Map.Entry<String, Integer> stat : set.getStats().entrySet()) {
-            String statName = translateKeys ? game.getLocale().get(title + "." + stat.getKey()) : stat.getKey();
+            String statName = game.getLocale().get("statistics.total." + stat.getKey());
             String statValue = Integer.toString(stat.getValue());
 
-            if (title.equals("statistics.total") && stat.getKey().equals("time")) {
-                int minutes = stat.getValue() / 60;
-                int seconds = stat.getValue() % 60;
-                statValue = minutes + ":" + (seconds > 10 ? seconds : "0" + seconds);
+            if (stat.getKey().equals("time")) {
+                statValue = secondsToTime(stat.getValue());
             }
 
-            add(new Label(statName, data)).fillX();
-            add(new Label(statValue, data)).align(Align.right).expandX().row();
+            total.add(new Label(statName, data)).align(Align.left).fillX();
+            total.add(new Label(statValue, data)).align(Align.right).expandX().row();
         }
+
+        return total;
     }
 
+    private Table addColorData() {
+        Table color = new Table();
+        color.add(new Label(game.getLocale().get("statistics.color"), this.title)).colspan(4).row();
+
+        // Put the data in a TreeMap. TreeMaps keep order. Use reverse to show biggers on left.
+        Map<Integer, String> colorScore = new TreeMap<>(Collections.reverseOrder());
+        colorScore.put(game.statistics.getColorData().getValue("red"), "red");
+        colorScore.put(game.statistics.getColorData().getValue("green"), "green");
+        colorScore.put(game.statistics.getColorData().getValue("blue"), "blue");
+        colorScore.put(game.statistics.getColorData().getValue("yellow"), "yellow");
+
+
+        color.defaults().expandX().fillX().align(Align.center).size(60).padTop(5);
+        for (Map.Entry<Integer, String> entry : colorScore.entrySet()) {
+            color.add(new Image(game.getBallAtlas().findRegion("ball_" + entry.getValue())));
+        }
+        color.row();
+        for (Map.Entry<Integer, String> entry : colorScore.entrySet()) {
+            Label label = new Label(entry.getKey().toString(), data);
+            label.setAlignment(Align.center);
+            color.add(label);
+        }
+        color.row();
+
+        return color;
+    }
+
+    private Table addSizesData() {
+        Table sizes = new Table();
+        sizes.add(new Label(game.getLocale().get("statistics.sizes"), this.title)).colspan(4).row();
+
+        Map<String, Integer> unsortedScores = game.statistics.getSizesData().getStats();
+        Map<Integer, String> sizesScore = new TreeMap<>(Collections.reverseOrder());
+        for (Map.Entry<String, Integer> score : unsortedScores.entrySet()) {
+            sizesScore.put(score.getValue(), score.getKey());
+        }
+
+        int highestValue = -1;
+        Drawable bar = game.getSkin().newDrawable("pixel", Color.WHITE);
+        for (Map.Entry<Integer, String> score : sizesScore.entrySet()) {
+            // Put the highest value. This should be done using the first item.
+            if (highestValue == -1) {
+                highestValue = score.getKey();
+            }
+
+            sizes.add(new Label(score.getValue(), data)).align(Align.left).fillX();
+            sizes.add(new Label(score.getKey().toString(), data)).align(Align.right).padRight(10).expandX();
+
+            // Add a bar to the right
+            float percentage = (float) score.getKey() / highestValue;
+            sizes.add(new Image(bar)).width(240 * percentage).padLeft(240 * (1 - percentage)).padBottom(5).fill().row();
+        }
+
+        return sizes;
+    }
+
+    private static String secondsToTime(int seconds) {
+        if (seconds < 60) {
+            return Integer.toString(seconds);
+        } else {
+            String min = Integer.toString(seconds / 60);
+            String sec = Integer.toString(seconds % 60);
+            if (sec.length() == 1) {
+                sec = "0" + sec;
+            }
+            return min + ":" + sec;
+        }
+    }
 }
