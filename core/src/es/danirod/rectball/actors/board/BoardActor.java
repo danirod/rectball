e5cd3c4854a0a7a5b1dd50d831149014781bfd66
Dani Rodríguez <danirod@outlook.com>
2015-10-14 14:31:09 +0200

    Make shake a returnable action
    Just like with hide and show board, don't apply actions inside
    actor classes, always return them and let the stage apply them.

--- core/src/es/danirod/rectball/actors/board/BoardActor.java
+++ core/src/es/danirod/rectball/actors/board/BoardActor.java
@@ -166,17 +166,23 @@ public class BoardActor extends Table {
         return resizeBalls(bounds, 1, 0.15f);
     }
 
-    public void shake(float shakiness) {
-        shake(new Bounds(0, 0, board.getSize() - 1, board.getSize() - 1), shakiness);
+    public Action shake(float shakiness, int times) {
+        return shake(new Bounds(0, 0, board.getSize() - 1, board.getSize() - 1), shakiness, times);
     }
 
-    public void shake(Bounds region, float shakiness) {
-        for (int y = region.minY; y <= region.maxY; y++)
-            for (int x = region.minX; x <= region.maxX; x++)
-                actors[x][y].addAction(Actions.repeat(5, Actions.sequence(
-                        Actions.moveBy(shakiness / 2, 0, 0.05f),
-                        Actions.moveBy(-shakiness, 0, 0.1f),
-                        Actions.moveBy(shakiness / 2, 0, 0.05f)
-                )));
+    public Action shake(final Bounds region, final float shakiness, final int times) {
+        Action shakingAction = Actions.run(new Runnable() {
+            @Override
+            public void run() {
+                for (int y = region.minY; y <= region.maxY; y++)
+                    for (int x = region.minX; x <= region.maxX; x++)
+                        actors[x][y].addAction(Actions.repeat(times, Actions.sequence(
+                                Actions.moveBy(shakiness / 2, 0, 0.05f),
+                                Actions.moveBy(-shakiness, 0, 0.1f),
+                                Actions.moveBy(shakiness / 2, 0, 0.05f)
+                        )));
+            }
+        });
+        return Actions.sequence(shakingAction, Actions.delay(times * 0.2f));
     }
 }
