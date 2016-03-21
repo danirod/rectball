72f87e9a699411d54845787dc62e658cb06ffea9
Dani Rodríguez <danirod@outlook.com>
2016-03-21 20:58:14 +0100

    Bump version to 0.4.7

--- core/src/main/java/es/danirod/rectball/Constants.java
+++ core/src/main/java/es/danirod/rectball/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
      * I'm still checking looking for a platform that works on any
      * platform.
      */
-    public static final String VERSION = "0.4.6";
+    public static final String VERSION = "0.4.7";
 
     /**
      * Whether the game is running in debug mode or not. Debug mode can be
