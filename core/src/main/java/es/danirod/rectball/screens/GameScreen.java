1d06d5cae0beaab2cf893ef7d24b4c9549fd3231
Dani Rodríguez <danirod@outlook.com>
2016-07-24 20:59:33 +0200

    Add new board screen (no logic behind)

--- core/src/main/java/es/danirod/rectball/screens/GameScreen.java
+++ core/src/main/java/es/danirod/rectball/screens/GameScreen.java
@@ -3,6 +3,8 @@ package es.danirod.rectball.screens;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 import es.danirod.rectball.RectballGame;
+import es.danirod.rectball.scene2d.actors.ScoreboardActor;
+import es.danirod.rectball.scene2d.actors.TableroActor;
 import es.danirod.rectball.scene2d.actors.TimeActor;
 
 /**
@@ -11,6 +13,10 @@ import es.danirod.rectball.scene2d.actors.TimeActor;
 public class GameScreen extends AbstractScreen {
 
     private TimeActor time;
+
+    private ScoreboardActor scoreboard;
+
+    private TableroActor tablero;
 
     public GameScreen(RectballGame game) {
         super(game);
@@ -29,14 +35,23 @@ public class GameScreen extends AbstractScreen {
     @Override
     void setUpInterface(Table table) {
         this.time = new TimeActor(game);
+        this.scoreboard = new ScoreboardActor(game);
+        this.tablero = new TableroActor(game);
         table.setDebug(true);
+        tablero.setDebug(true);
+        table.add(scoreboard).fillX().expandX().height(80).row();
         table.add(time).fillX().expandX().height(80).row();
+        table.add(tablero).expand().fill();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         game.getState().tick(delta);
+
+        if (game.getState().getRemainingTime() < 0) {
+            game.setScreen(game.getScreen(Screens.GAME_OVER));
+        }
     }
 
     @Override
