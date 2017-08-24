cd8782cc400faf5a0232d4be3f59cd43c9ac7911
Dani Rodríguez <dani@danirod.es>
2017-08-24 13:42:50 +0200

    Manually set the immersive screen flag

--- app/src/main/java/es/danirod/rectball/android/AndroidLauncher.java
+++ app/src/main/java/es/danirod/rectball/android/AndroidLauncher.java
@@ -18,7 +18,9 @@
 
 package es.danirod.rectball.android;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -52,11 +54,6 @@ public class AndroidLauncher extends AndroidApplication {
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-
-        if (platform.preferences().getBoolean("fullscreen")) {
-            config.useImmersiveMode = true;
-            config.hideStatusBar = true;
-        }
 
         if (savedInstanceState != null) {
             Json json = new Json();
@@ -111,5 +108,26 @@ public class AndroidLauncher extends AndroidApplication {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         platform.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && android.os.Build.VERSION.SDK_INT >= 19) {
+            setFullscreenFlags();
+        }
+    }
+
+    @TargetApi(19) // KitKat
+    private void setFullscreenFlags() {
+        if (platform != null && platform.preferences().getBoolean("fullscreen")) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
